35910e809cc0585953fe1285553ea8eff23f6c0a
kevinbdc <89201245+kevinbdc@users.noreply.github.com>
2022-05-20 15:11:43 -0300

    Add files via upload

---
+++ GestoA/Visualizar.m
@@ -0,0 +1,20 @@
+% close all; clear all; clc;
+close all% GestoG : Itau com mao em altura média
+%Gesto H : Itau com a mao em altura alta
+% Abre o arquivo do Gesto na Path Folder e roda o cògido
+
+%A=load('GestosCompletos.mat')
+% B = a.B;
+%SJ=A.SJ;
+
+% B = Gesto_Compara
+
+%% Remover os zeros
+
+for k = 1:3:length(SJ)
+    h = plot (SJ(:,k), SJ(:,k+1), '.','MarkerSize',10);
+    axis([-2.1 2.1 -1.5 1.5])
+        pause(0.05) % Mudar o tempo altera o tempo entre os frames
+end
+
+size(SJ)
